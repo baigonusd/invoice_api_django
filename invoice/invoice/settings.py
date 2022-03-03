a27d695957fb5bf262623e07ee8b3889c15354fe
baigonusd <baigonus.d@gmail.com>
2022-03-03 15:44:33 +0600

    Dockerized

--- invoice/invoice/settings.py
+++ invoice/invoice/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["0.0.0.0", os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")]
 
 
 # Application definition
